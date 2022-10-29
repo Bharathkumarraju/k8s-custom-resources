d15b2fa0e47c71c47bf684af2e9e56fbf5a7c6e7
Bharath <bharath.dasaraju@advancegroup.com>
2022-10-30 06:18:18 +0800

    :seedling: introduction of custom resources in k8s :seedling:

---
+++ 1.k8s-custom-resource-intro.sh
@@ -0,0 +1,14 @@
+k8s is a powerful container orchestrator right out of the box.
+
+but what do you do when you need to customize your kubernetes installation.
+
+custom resources are key when it comes to extending kubernetes. Custom resources allow you to build custom functionality on top of kubernetes.
+
+1. Custom Resources
+2. Custom Controllers
+3. Kubernetes Operators
+4. Custom Schedulers
+
+Needs some familiarity the programming languages like Go.
+
+
